f7666122d00432c45e6848a3c8c5f03e18af5e36
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-05-20 17:43:26 +0200

    Update lua script

--- slack.lua
+++ slack.lua
@@ -1,37 +1,3 @@
-local function escape(s, in_attribute)
-  return s:gsub("[<>&\"']",
-    function(x)
-        return x
-    end)
-end
-
--- Helper function to convert an attributes table into
--- a string that can be put into HTML tags.
-local function attributes(attr)
-  local attr_table = {}
-  for x,y in pairs(attr) do
-    if y and y ~= "" then
-      table.insert(attr_table, ' ' .. x .. '="' .. escape(y,true) .. '"')
-    end
-  end
-  return table.concat(attr_table)
-end
-
--- Run cmd on a temporary file containing inp and return result.
-local function pipe(cmd, inp)
-  local tmp = os.tmpname()
-  local tmph = io.open(tmp, "w")
-  tmph:write(inp)
-  tmph:close()
-  local outh = io.popen(cmd .. " " .. tmp,"r")
-  local result = outh:read("*all")
-  outh:close()
-  os.remove(tmp)
-  return result
-end
-
--- Table to store footnotes, so they can be included at the end.
-local notes = {}
 
 -- Blocksep is used to separate block elements.
 function Blocksep()
@@ -50,81 +16,19 @@ function Doc(body, metadata, variables)
     table.insert(buffer, s)
   end
   add(body)
-  if #notes > 0 then
-    add('<ol class="footnotes">')
-    for _,note in pairs(notes) do
-      add(note)
-    end
-    add('</ol>')
-  end
   return table.concat(buffer,'\n')
 end
 
--- The functions that follow render corresponding pandoc elements.
--- s is always a string, attr is always a table of attributes, and
--- items is always an array of strings (the items in a list).
--- Comments indicate the types of other variables.
-
 function Str(s)
-  return escape(s)
+  return s
 end
 
 function Space()
   return " "
 end
 
-function Subscript(s)
-  return "<sub>" .. s .. "</sub>"
-end
-
-function Superscript(s)
-  return "<sup>" .. s .. "</sup>"
-end
-
-function SmallCaps(s)
-  return '<span style="font-variant: small-caps;">' .. s .. '</span>'
-end
-
 function Link(s, src, tit)
   return src
-end
-
-function Image(s, src, tit)
-  return "<img src='" .. escape(src,true) .. "' title='" ..
-         escape(tit,true) .. "'/>"
-end
-
-function InlineMath(s)
-  return "\\(" .. escape(s) .. "\\)"
-end
-
-function DisplayMath(s)
-  return "\\[" .. escape(s) .. "\\]"
-end
-
-function Note(s)
-  local num = #notes + 1
-  -- insert the back reference right before the final closing tag.
-  s = string.gsub(s,
-          '(.*)</', '%1 <a href="#fnref' .. num ..  '">&#8617;</a></')
-  -- add a list item with the note to the note table.
-  table.insert(notes, '<li id="fn' .. num .. '">' .. s .. '</li>')
-  -- return the footnote reference, linked to the note.
-  return '<a id="fnref' .. num .. '" href="#fn' .. num ..
-            '"><sup>' .. num .. '</sup></a>'
-end
-
-function Span(s, attr)
-  return "<span" .. attributes(attr) .. ">" .. s .. "</span>"
-end
-
-function Cite(s, cs)
-  local ids = {}
-  for _,cit in ipairs(cs) do
-    table.insert(ids, cit.citationId)
-  end
-  return "<span class=\"cite\" data-citation-ids=\"" .. table.concat(ids, ",") ..
-    "\">" .. s .. "</span>"
 end
 
 function Plain(s)
@@ -135,8 +39,6 @@ function Para(s)
   return s .. "\n"
 end
 
--- lev is an integer, the header level.
-
 function HorizontalRule()
   return "---"
 end
@@ -145,77 +47,8 @@ function CodeBlock(s, attr)
   return "```\n" .. s .. "```\n"
 end
 
-function OrderedList(items)
-  local buffer = {}
-  for _, item in pairs(items) do
-    table.insert(buffer, "* " .. item .. "")
-  end
-  return table.concat(buffer, "\n")
-end
-
-
--- Caption is a string, aligns is an array of strings,
--- widths is an array of floats, headers is an array of
--- strings, rows is an array of arrays of strings.
-function Table(caption, aligns, widths, headers, rows)
-  local buffer = {}
-  local function add(s)
-    table.insert(buffer, s)
-  end
-  add("<table>")
-  if caption ~= "" then
-    add("<caption>" .. caption .. "</caption>")
-  end
-  if widths and widths[1] ~= 0 then
-    for _, w in pairs(widths) do
-      add('<col width="' .. string.format("%d%%", w * 100) .. '" />')
-    end
-  end
-  local header_row = {}
-  local empty_header = true
-  for i, h in pairs(headers) do
-    local align = html_align(aligns[i])
-    table.insert(header_row,'<th align="' .. align .. '">' .. h .. '</th>')
-    empty_header = empty_header and h == ""
-  end
-  if empty_header then
-    head = ""
-  else
-    add('<tr class="header">')
-    for _,h in pairs(header_row) do
-      add(h)
-    end
-    add('</tr>')
-  end
-  local class = "even"
-  for _, row in pairs(rows) do
-    class = (class == "even" and "odd") or "even"
-    add('<tr class="' .. class .. '">')
-    for i,c in pairs(row) do
-      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. '</td>')
-    end
-    add('</tr>')
-  end
-  add('</table')
-  return table.concat(buffer,'\n')
-end
-
--- The following code will produce runtime warnings when you haven't defined
--- all of the functions you need for the custom writer, so it's useful
--- to include when you're working on a writer.
-local meta = {}
-meta.__index =
-  function(_, key)
-    io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
-    return function() return "" end
-  end
-setmetatable(_G, meta)
-
-
-
-function LineBreak()
-  -- return "\n"
-  return "<linebreak>"
+function SoftBreak()
+  return "\n"
 end
 
 function Emph(s)
@@ -231,17 +64,36 @@ function Strikeout(s)
 end
 
 function Code(s, attr)
-  return '```' .. s .. '```'
+  return string.format("`%s`", s)
 end
 
 function Header(lev, s, attr)
-  return '*# ' .. s .. '*\n'
+  return '*'.. string.rep('#', lev) ..' ' .. s .. '*\n'
 end
 
 function BulletList(items)
   local buffer = {}
   for _, item in pairs(items) do
-    table.insert(buffer, "• " .. item)
+    table.insert(buffer, "  • " .. item)
   end
   return table.concat(buffer, "\n") .. "\n"
 end
+
+function OrderedList(items, v, x, z)
+  local buffer = {}
+  for i, item in pairs(items) do
+    table.insert(buffer, "  ".. i .. ". " .. item .. "")
+  end
+  return table.concat(buffer, "\n") .. "\n"
+end
+
+-- The following code will produce runtime warnings when you haven't defined
+-- all of the functions you need for the custom writer, so it's useful
+-- to include when you're working on a writer.
+local meta = {}
+meta.__index =
+  function(_, key)
+    io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
+    return function() return "" end
+  end
+setmetatable(_G, meta)
